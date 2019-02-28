4e3a2324aef556ec01de9b6d287971f900cd8cfc
Andrew Gurinovich <twain575@gmail.com>
2019-02-28 20:50:41 +0300

    solution task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let triangle = 0;
+  preferences.unshift(0);
+    
+  for(let i = 1; i < preferences.length; i++) {
+      
+    let spichoneeA = preferences[i]; // 6
+    let spichoneeB = preferences[spichoneeA]; // 20
+    let spichoneeC = preferences[spichoneeB]; // 1
+
+    if (i === spichoneeC) {
+      triangle ++
+    }
+  }
+  return Math.round(triangle / 3);
 };
